5dac48d911a130ad5eb9dcf1fa77460d22b1cbc4
Paulo Alves <paulorsaf@gmail.com>
2022-08-19 15:39:52 +0400

    Purchase repository: Find all by company and user
    Formatting createdAt date as yyyy-MM-dd HH:mm:ss

--- src/modules/purchases/repositories/purchase.repository.ts
+++ src/modules/purchases/repositories/purchase.repository.ts
@@ -6,6 +6,29 @@ import { Purchase } from '../model/purchase.model';
 @Injectable()
 export class PurchaseRepository {
 
+    findAll(query: FindAllQuery) {
+        return admin.firestore()
+            .collection('purchases')
+            .where("companyId", "==", query.companyId)
+            .where("userId", "==", query.userId)
+            // .orderBy("createdAt", "desc")
+            .get()
+            .then(snapshot => {
+                return snapshot.docs.map(d => {
+                    const data = d.data();
+                    return new Purchase({
+                        companyId: data.companyId,
+                        userId: data.userId,
+                        id: data.id,
+                        address: data.address,
+                        payment: data.payment,
+                        status: data.status,
+                        createdAt: data.createdAt
+                    })
+                })
+            })
+    }
+
     save(purchase: Purchase) {
         const purchaseObj = JSON.parse(JSON.stringify(purchase));
 
@@ -13,10 +36,15 @@ export class PurchaseRepository {
             .collection('purchases')
             .add({
                 ...purchaseObj,
-                createdAt: format(new Date(), 'yyyy-MM-dd HH:mm:ss:SSSS'),
+                createdAt: format(new Date(), 'yyyy-MM-dd HH:mm:ss'),
                 status: "CREATED"
             })
             .then(response => response.id)
     }
 
+}
+
+type FindAllQuery = {
+    companyId: string;
+    userId: string;
 }
