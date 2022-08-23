6cab4cae539535b4441eff7ba6de8ea41a9e0270
Paulo Alves <paulorsaf@gmail.com>
2022-08-23 16:03:04 +0200

    Removing JWT strategy from find company

--- src/modules/company/company.controller.ts
+++ src/modules/company/company.controller.ts
@@ -2,12 +2,11 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthCompany } from '../../authentication/decorators/company.decorator';
 import { Company } from '../../authentication/model/company';
 import { CompanyStrategy } from '../../authentication/guards/company.strategy';
-import { JwtStrategy } from '../../authentication/guards/jwt.strategy';
 
 @Controller('companies')
 export class CompanyController {
 
-  @UseGuards(CompanyStrategy, JwtStrategy)
+  @UseGuards(CompanyStrategy)
   @Get()
   find(@AuthCompany() company: Company) {
     return company;
