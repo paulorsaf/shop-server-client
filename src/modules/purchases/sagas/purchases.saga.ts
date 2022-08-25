94beb8ede63e61983990adf71301a4c674fcf110
Paulo Alves <paulorsaf@gmail.com>
2022-08-25 10:36:54 +0200

    Removing unecessary imports

--- src/modules/purchases/sagas/purchases.saga.ts
+++ src/modules/purchases/sagas/purchases.saga.ts
@@ -1,12 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { ICommand, ofType, Saga } from "@nestjs/cqrs";
 import { map, Observable } from "rxjs";
-import { Product } from "../../stocks/commands/decrease-amount-on-stock-options/model/product.model";
-import { Stock } from "../../stocks/commands/decrease-amount-on-stock-options/model/stock.model";
 import { DecreaseStockOptionsOnPurchaseCommand } from "../../stocks/commands/decrease-amount-on-stock-options/decrease-stock-options-on-purchase.command";
 import { PurchaseCreatedEvent } from "../commands/create-purchase/events/purchase-created.event";
-import { PurchasePayment } from "../../payment/model/purchase/puchase-payment.model";
-import { Payment } from "../../payment/model/payment/payment.model";
 import { SelectPurchasePaymentCommand } from "../../payment/commands/select-payment/select-purchase-payment.command";
 import { PurchasePaymentRetriedEvent } from "../events/purchase-payment-retried.event";
 
