53cfd6ae7587c8d2445293e3afd1569e218a35b7
Paulo Alves <paulorsaf@gmail.com>
2022-12-13 19:02:06 +0100

    Avoiding unecessary env errors

--- src/modules/payment/factories/payment.factory.ts
+++ src/modules/payment/factories/payment.factory.ts
@@ -42,10 +42,14 @@ export class PaymentFactory {
     }
 
     private getFromEnv(companyId: string, key: string) {
-        console.log('### process.env', JSON.stringify(process.env));
-        console.log('### env.parsed', JSON.stringify(this.env.parsed));
-        return process.env[`_${companyId.toUpperCase()}_${key}`] ||
-            this.env.parsed[`_${companyId.toUpperCase()}_${key}`];
+        const prop = `_${companyId.toUpperCase()}_${key}`;
+        if (process.env.hasOwnProperty(prop)) {
+            return process.env[`_${companyId.toUpperCase()}_${key}`];
+        }
+        if (this.env.parsed && this.env.parsed.hasOwnProperty(prop)) {
+            this.env.parsed[`_${companyId.toUpperCase()}_${key}`]
+        }
+        return "";
     }
 
 }
